ea2c8330228db4e4e568ec07676e927716e498de
colinl <clanlaw@gmail.com>
2024-06-16 10:39:18 +0100

    Further refactoring. Only send modified properties to client

--- nodes/ui-gauge-classic.js
+++ nodes/ui-gauge-classic.js
@@ -19,23 +19,26 @@ module.exports = function (RED) {
             onInput: function (msg, send, done) {
                 // pick up existing stored data
                 let storedData = base.stores.data.get(node.id)
-                console.log(`onInput storedData: ${JSON.stringify(storedData)}\n\n`)
+                //console.log(`onInput storedData: ${JSON.stringify(storedData)}\n\n`)
 
                 // does msg.payload exist?
                 if (typeof msg.payload != "undefined") {
                     // yes so update value from payload
                     //console.log(`onInput, needles: ${JSON.stringify(needles)}`)
+                    let needle = null
                     if (storedData.needles.length === 1) {
                         // only one needle so ignore topic
-                        storedData.needles[0].value = msg.payload
+                        needle = storedData.needles[0]
                     } else {
                         // find the needle with the matching topic
-                        const needle = storedData.needles.find((element) => element.topic === msg.topic);
-                        if (needle) {
-                            needle.value = msg.payload
-                        } else {
-                            console.log(`Classic gauge - msg with no matching needle topic ${msg.topic}`)
-                        }
+                        needle = storedData.needles.find((element) => element.topic === msg.topic);
+                    }
+                    if (needle) {
+                        needle.value = msg.payload
+                        // pick up the needles array for sending to clients
+                        msg.needles = storedData.needles
+                    } else {
+                        console.log(`Classic gauge - msg with no matching needle topic ${msg.topic}`)
                     }
                 }
 
@@ -45,22 +48,20 @@ module.exports = function (RED) {
                     storedData.ui_update ??= {}    // initialise if necessary
                     // merge in data from this message
                     storedData.ui_update = {...storedData.ui_update, ...msg.ui_update}
+                } else {
+                    // delete any msg.ui_update so don't need to validate in clients
+                    delete msg.ui_update
                 }
                 // pick up msg.formattedValue if present and is a string
                 if (typeof msg.formattedValue === "string") {
                     storedData.formattedValue = msg.formattedValue
                 }
 
-                // store the latest value in our Node-RED datastore
+                // store the latest full set of values in our Node-RED datastore
                 //console.log(`leaving onInput storedData: ${JSON.stringify(storedData)}\n\n`)
                 base.stores.data.save(base, node, storedData)
-                // send it to any connected nodes in Node-RED
-                // hack to work around the fact that send() always sends the original msg object
-                for (const property in storedData) {
-                    msg[property] = storedData[property]
-                }
+                // send the message with modified properties to the clients
                 send(msg)
-                //send(storedData)
             },
             onSocket: {
                 /*
